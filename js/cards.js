ba536e70c9b65769dcb60f0d6ef135029577648e
Tony Grimes <acidtone@tonygrimes.com>
2022-08-20 12:00:10 -0600

    added cards stub array

---
+++ js/cards.js
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    label: 'Corner Store',
+    type: 'discard',
+    cost: 3,
+    description: '+1 [victory-point]'
+  },
+  {
+    label: 'Flame Thrower',
+    type: 'discard',
+    cost: 3,
+    description: 'All other Monsters lose 2 [hearts].'
+  },
+  {
+    label: 'Evacuation Orders',
+    type: 'discard',
+    cost: 7,
+    description: 'All other Monsters lose 5 [victory-points]'
+  },
+  {
+    label: 'High Altitude Bombing',
+    type: 'discard',
+    cost: 4,
+    description: 'All Monsters (including you) lose 3 [hearts].'
+  },
+  {
+    label: 'Gas Refinery',
+    type: 'discard',
+    cost: 3,
+    description: '+2 [victory-points] and all other Monsters lose 3 [hearts].'
+  },
+]
