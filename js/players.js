93a7e92c0fd766ffcc2fdfed58329aa1c6c38961
Ashlyn Knox <ashlyn@lilyx.io>
2022-08-27 15:07:13 -0600

    changed default values

--- js/players.js
+++ js/players.js
@@ -17,8 +17,8 @@ class Player {
 }
 
 function Player(name, character) {
-  this.name = name ? name : ""
-  this.character = character ? character : ""
+  this.name = name ? name : "player"
+  this.character = character ? character : "monster"
   this.health = 10
   this.victoryPoints = 0
   this.energy = 0
