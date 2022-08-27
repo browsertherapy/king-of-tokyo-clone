a92c2ab6c2e748c4c9488106c7c907a873d64a53
Ashlyn Knox <ashlyn@lilyx.io>
2022-08-27 15:07:35 -0600

    removed class based player creator

--- js/players.js
+++ js/players.js
@@ -1,21 +1,3 @@
-class Player {
-  constructor(
-    name = "",
-    character = "",
-    health = 10,
-    victoryPoints = 0,
-    energy = 0,
-    cards = []
-  ) {
-    this.name = name
-    this.character = character
-    this.health = health
-    this.victoryPoints = victoryPoints
-    this.energy = energy
-    this.cards = cards
-  }
-}
-
 function Player(name, character) {
   this.name = name ? name : "player"
   this.character = character ? character : "monster"
